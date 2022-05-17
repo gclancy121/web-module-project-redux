de1647fd9b807f44e90cb188376975b530dbd314
Gage Clancy <gclancy121@gmail.com>
2022-05-17 15:48:41 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -9,9 +9,20 @@ import App from './components/App';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 
+
+
+let store
+export const resetStore = () => {
+  const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
+  store = createStore(reducer, composeEnhancers())
+}
+resetStore()
+
 ReactDOM.render(
-  <Router>
-      <App />
-  </Router>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+        <App />
+    </Router>
+  </Provider>,
+    document.getElementById('root')
 );
